d54dbaa5d317a3ae68a98a6a6c1f9fb24e2f5071
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-18 15:43:02 +0300

    Added the imports of a few components and icons

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { BsCurrencyDollar } from 'react-icons/bs'
+import { GoPrimitiveDot } from 'react-icons/go'
+import { Stacked, PieChart, Button, SparkLine } from '../components'
 
 const Ecommerce = () =>
 {
