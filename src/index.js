a75932b625425aec20c5e373c43a5a3a706cfc03
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-18 12:12:08 +0300

    Wrapped the entire application with the COntextProvider

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app'
 import './index.css'
+import { ContextProvider } from './context/ContextProvider'
 
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+    <ContextProvider>
+        <App />
+    </ContextProvider>
+    , document.getElementById('root'))
