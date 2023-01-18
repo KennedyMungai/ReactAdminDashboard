464d826ed93bdbc8fbfa386337fd94c078d8d61f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-18 12:16:22 +0300

    Assigned activeMenu to the useStateContext hook

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import { SiShopware } from 'react-icons/si'
 import { MdOutlineCancel } from 'react-icons/md'
@@ -9,7 +9,7 @@ import { useStateContext } from '../context/ContextProvider'
 
 const Sidebar = () =>
 {
-    const activeMenu = true
+    const { activeMenu } = useStateContext()
 
     const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2'
     const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark: text-gray-200 dark:hover: text-black hover: bg-light-gray m-2'
