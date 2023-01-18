ddc0f7b0d8ef959eb0121c94caef25ecd668ef8c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-18 15:21:26 +0300

    Passed the screensize and the setscreenSize values of the provider

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,7 +32,7 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
 
 const Navbar = () =>
 {
-    const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick } = useStateContext()
+    const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setscreenSize } = useStateContext()
 
     return (
         <div className='flex justify-between p-2 md: mx-6 relative'>
