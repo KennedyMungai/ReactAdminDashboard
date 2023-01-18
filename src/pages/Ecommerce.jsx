bdb39c02742598fb61e6a45300091dfaa1423665
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-18 15:44:35 +0300

    imported dummy data

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { BsCurrencyDollar } from 'react-icons/bs'
 import { GoPrimitiveDot } from 'react-icons/go'
 import { Stacked, PieChart, Button, SparkLine } from '../components'
+import { earningData, SparklineAreaData, ecomPieChartData } from '../data/dummy'
+
 
 const Ecommerce = () =>
 {
