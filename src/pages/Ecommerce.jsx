8b85496b349af58238b84f55b9cc8376daeb84d8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-18 15:45:45 +0300

    Imported the useStateContext

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -3,6 +3,7 @@ import { BsCurrencyDollar } from 'react-icons/bs'
 import { GoPrimitiveDot } from 'react-icons/go'
 import { Stacked, PieChart, Button, SparkLine } from '../components'
 import { earningData, SparklineAreaData, ecomPieChartData } from '../data/dummy'
+import { useStateContext } from '../context/ContextProvider'
 
 
 const Ecommerce = () =>
