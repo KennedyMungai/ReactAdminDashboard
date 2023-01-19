db71d93a090bf765ba230491797d37213defcd79
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-19 14:51:02 +0300

    Made the text dynamic

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -8,7 +8,7 @@ const Button = ({ bgColor, color, size, text, borderRadius }) =>
             style={{ backgroundColor: bgColor, color, borderRadius }}
             className={`text-${size} p-3 hover: drop-shadow-xl`}
         >
-
+            {text}
         </button>
     )
 }
