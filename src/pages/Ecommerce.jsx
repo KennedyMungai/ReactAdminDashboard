84d0e0095f51460e7480be0614fc0109e4a3a668
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-19 14:59:02 +0300

    Dynamically added buttons to it

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -26,7 +26,19 @@ const Ecommerce = () =>
                         />
                     </div>
                 </div>
-
+                <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
+                    {earningData.map((item) =>
+                    {
+                        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl">
+                            <button
+                                type="button"
+                                style={{color: item.iconColor, backgroundColor: item.iconBg}}
+                            >
+{ite}
+                            </button>
+                        </div>
+                    })}
+                </div>
             </div>
         </div>
     )
